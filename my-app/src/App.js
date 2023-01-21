d3ed0913ba1f00d73dd69db540c35de2b2d61849
Andy L <andyjiaanli@gmail.com>
2023-01-21 14:00:49 -0800

    fixed App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <h1>Hello World</h1>
-      <img src=".\public\ms-icon-310x310.png" alt="logo"></img>
-=======
       <h1>Hello World</h1>  
->>>>>>> 347e5961298ef82262e0a966202513dd660b2836
     </div>
   );
 }
