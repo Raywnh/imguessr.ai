c0eb5fb4bd986502a19fdc00ea5ce77008d5b44f
Miguel menard <miguel.menard@me.com>
2023-01-21 13:19:32 -0800

    stuff

--- server/server.js
+++ server/server.js
@@ -2,4 +2,8 @@ const express = require('express')
 
 const app = express()
 
-app.listen(5000, () => {console.log("Server started on Port 5000")})
+const openai = new OpenAIApi(configuration);
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+  
