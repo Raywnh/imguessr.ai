d6f6b9a3817bff9fbc95401d58e6d5c3b4ee3993
Miguel menard <miguel.menard@me.com>
2023-01-21 21:50:28 -0800

    Css Styliing and Website visual Modifications

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,27 +13,25 @@ function App() {
   return (
     <div>
       {/* Header */}
-      <header className="container green center">
+      <div className="container green center">
         <img src={require('./assets/imguessr-header-banner.png')} alt="The banner of the website" />
-        <p className="xlarge">Choose your difficulty:</p>
-        <button
-          className="button dark-yellow padding-large large margin"
-          id="normalButton"
-          onClick={goToNormal}
-        >
-          Normal
-        </button>
-        <button className="button dark-yellow padding-large large margin">
-          Extremely Hard
-        </button>
-      </header>
+        
+      </div>
 
       {/* First Grid */}
       {pages == 0 ? (
-        <div className="row-padding padding-64 container">
+        <div className="row-padding padding-64 container sage main">
           <div className="content">
-            <div className="twothird">
-              <h1>Difficulties Explained</h1>
+            <div className="twothird mustard padding-16">
+              <h1>How to Play the Game!</h1>
+              <p className="padding-16">
+                Welcome to IMGUESSR.IO, a game that utilizes DALL-E 2 By OpenAI
+                to generate random images. DALL_E 2 has gone rogue and is generating random images without your command. 
+                Your task is to guess the prompt that DALL-E was given in order to generate its image.! All difficulties
+                give you 60 seconds to guess the image, with new images being
+                added every 15 seconds for extra help. Guess the prompt in 60
+                seconds to win!
+              </p>
               <h5 className="padding-16">
                 <strong>Normal</strong> - 1 word prompt for fun, easy, and
                 stress free gameplay
@@ -42,35 +40,47 @@ function App() {
                 <strong>Extremely Hard</strong> - 2 word prompt that is
                 completely random. For the brave.
               </h5>
-              <p className="padding-16">
-                Welcome to IMGUESSR.IO, a game that utilizes DALL-E 2 By OpenAI
-                to generate random images for you to guess. All difficulties
-                give you 60 seconds to guess the image, with new images being
-                added every 15 seconds for extra help. Guess the prompt in 60
-                seconds to win!
-              </p>
+              
+              <p className="xlarge">Choose your difficulty:</p>
+              <div>
+                    <button
+                  className="button sage"
+                  id="normalButton"
+                  onClick={goToNormal}
+                >
+                  Normal
+                </button>
+                <p> or </p>
+                <button className="button sage">
+                  Extremely Hard
+                </button>
+              </div>
             </div>
-          </div>
+            </div>
           {/* Footer */}
           <footer className="container padding-32 center opacity">
-            <p>
-              Made by{" "}
+            <p>Made by{"  "} </p>
+              
               <a href="https://github.com/rayzhou4" target="_blank">
-                Ray Z
+              <img src={require('./assets/git.png')} alt="Ray Z" id="logo"/>
+              <p>Ray Z</p>
               </a>{" "}
               ·{" "}
               <a href="https://github.com/Raywnh" target="_blank">
-                Ray H
+              <img src={require('./assets/git.png')} alt="Ray Z" id="logo"/>
+              <p>Ray H</p>
               </a>{" "}
               ·{" "}
               <a href="https://github.com/migies12" target="_blank">
-                Miguel M
+              <img src={require('./assets/git.png')} alt="Ray Z" id="logo"/>
+              <p>Miguel M</p>
               </a>{" "}
               ·{" "}
               <a href="https://github.com/AndyJLi0" target="_blank">
-                Andy L
+              <img src={require('./assets/git.png')} alt="Ray Z" id="logo"/>
+              <p>Andy L</p>
               </a>
-            </p>
+
           </footer>
         </div>
       ) : (
